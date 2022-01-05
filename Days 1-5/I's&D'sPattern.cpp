ed0e6a014cff8fe31e6a2f1addbb58db200806f0
rogue-codes-23 <97082391+rogue-codes-23@users.noreply.github.com>
2022-01-05 13:23:59 +0530

    Create I's&D'sPattern.cpp

---
+++ Days 1-5/I's&D'sPattern.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string print(string s){
+    string x = "";
+    int i=0,j=0;
+    int count=1;
+    while(i<=s.size()){
+        if(s[i]=='I' || i==s.size()){
+            while(count>=1){
+                if(count<=j) break;
+                char ch = count + '0';
+                x.push_back(ch);
+                count--;
+            }
+            j = i+1;
+            count = i+1;
+        }
+        count++;
+        i++;
+    }
+    return x;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    string ans = print(s);
+    cout<<ans;
+}
